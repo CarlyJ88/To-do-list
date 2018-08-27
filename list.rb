f2bdcade7f5853348608be9ee4911ab6d365b445
Carly <carly.jenkinson65@googlemail.com>
2018-08-27 18:32:37 +0100

    Listing to-do's and add item functionality implemented

--- list.rb
+++ list.rb
@@ -1,13 +1,17 @@
-
-def list_to_do
-  puts "Here are the lists"
+def list_to_do(to_do)
+  puts "I need to: "
+  to_do.each_with_index do |x, index|
+    puts "#{index}: #{x}"
+  end
 end
 
-def add_item
-  puts "Would you like to add an item to your list now?"
+def add_item(to_do)
+  puts "Please enter the item you would like to add to the list below."
+  item = $stdin.gets.chomp
+  to_do.push(item)
 end
 
-def delete_item
+def delete_item(to_do)
   puts "Are you sure you would like to delete this?"
 end
 
@@ -22,28 +26,27 @@ def select_item
   puts "Press 3 to delete an element from the list"
   puts "Press 4 to exit the program"
   puts "Press any other number if you would like to repeat the list"
-
-
-puts "> "
-
-number = $stdin.gets.chomp
+  puts ">"
+  $stdin.gets.chomp
 end
 
 def choice
-  loop do
-  number = select_item
+  to_do = ["Write CV", "update LinkedIn profile", "Buy another Olbas oil nasal stick", "Find a job in London", "Complete to-do list console application"]
 
-if number == "1"
-  list_to_do
-elsif number == "2"
-    add_item
-  elsif number == "3"
-      delete_item
+  loop do
+    number = select_item
+
+    if number == "1"
+      list_to_do(to_do)
+    elsif number == "2"
+      add_item(to_do)
+    elsif number == "3"
+      delete_item(to_do)
     elsif number == "4"
       bye
     end
     break if number=="4"
-end
+  end
 end
 
 choice
